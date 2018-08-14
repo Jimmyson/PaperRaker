9a9857f98f92408746b938467b9a79cd94cdcdb5
Jimmyson <jimmyson.2007@gmail.com>
2018-08-14 22:14:16 +1000

    Add Fluent Validation

---
+++ PaperRaker.Validation/AuthorValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using PaperRaker.Core.Model.System;
+
+namespace PaperRaker.Validation
+{
+    public class AuthorValidation : AbstractValidator<Author>
+    {
+        public AuthorValidation()
+        {
+            RuleFor(a => a.Id).NotNull().NotEmpty().NotEqual(0);
+            
+            RuleFor(a => a.Given).NotNull().NotEmpty();
+            RuleFor(a => a.Family).NotNull().NotEmpty();
+        }
+    }
+}
---
+++ PaperRaker.Validation/PaperValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PaperRaker.Core.Model.System;
+
+namespace PaperRaker.Validation
+{
+    public class PaperValidation : AbstractValidator<Paper>
+    {
+        public PaperValidation()
+        {
+            RuleFor(p => p.Id).NotNull().NotEmpty().NotEqual(0);
+            
+            RuleFor(p => p.Title).NotNull().NotEmpty();
+            RuleFor(p => p.PeerReviewed).NotNull().NotEmpty();
+            RuleFor(p => p.PageCount).NotNull().NotEmpty();
+
+            RuleForEach(p => p.Contributors).NotNull();
+        }
+    }
+}
